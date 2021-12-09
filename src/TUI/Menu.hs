bcffcfa4748765d0b8d72cd969cc1883875587c4
Dun Ma <madun1999@gmail.com>
2021-12-08 18:03:30 -0800

    Finish Menu

--- src/TUI/Menu.hs
+++ src/TUI/Menu.hs
@@ -3,20 +3,33 @@
 module TUI.Menu where
 
 import Brick
+    ( attrMap,
+      continue,
+      neverShowCursor,
+      AttrMap,
+      App(..),
+      EventM,
+      BrickEvent (VtyEvent, AppEvent),
+      Next,
+      Widget, on, halt, str, (<+>), showFirstCursor, vBox, vLimit, padTop, Padding (Pad) )
 
 import qualified Data.Text as T
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
 import qualified Graphics.Vty as V
-import Brick.Forms (newForm, Form, editTextField, renderForm, invalidFormInputAttr)
+import Brick.Forms (newForm, Form, editTextField, renderForm, invalidFormInputAttr, focusedFormInputAttr, handleFormEvent, (@@=))
 import Lens.Micro.TH (makeLenses)
+import Lens.Micro ((^.), (&), (.~))
+import Graphics.Vty (black, yellow, white, red)
+import Data.Text (empty)
+import Control.Monad.IO.Class (MonadIO(liftIO))
 
-type ServerIP = String
 
-type TableID = String 
+-------------- Menu Types ----------------
 
-data ConnectionState = 
+-- States
+data ConnectionState =
   Idle | Connecting | Error String | Success
   deriving (Show)
 
@@ -25,49 +38,100 @@ data ConnectionInfo = ConnectionInfo {
   , _tableID :: T.Text
 } deriving (Show)
 
-data MenuEvent = MenuEvent
-data MenuResourceName = 
-  MenuServerIPField | MenuTableIDField
-  deriving (Eq, Ord, Show)
-
 data MenuState = MenuState {
     _connectionInfo :: ConnectionInfo
   , _connectionState :: ConnectionState
-} deriving (Show)
+  , _connectionInfoForm :: Form ConnectionInfo MenuEvent MenuResourceName
+}
 
+-- Event
+type Connection = ()
+
+data MenuEvent = ConnectionSuccess Connection | ConnectionFailed String
+
+-- Resource
+data MenuResourceName =
+  MenuServerIPField | MenuTableIDField
+  deriving (Eq, Ord, Show)
+
+-- Convenient Lenses
 makeLenses ''ConnectionInfo
 makeLenses ''MenuState
 
+---------- Initial State for the menu --------------
 
+initialMenuState :: MenuState
+initialMenuState = MenuState ci Idle (makeConnectionForm ci)
+                    where ci = ConnectionInfo empty empty
+
+
+---------- Brick App ------------
 
 app :: App MenuState MenuEvent MenuResourceName
-app = App { appDraw = drawMenu 
-          , appChooseCursor = neverShowCursor
+app = App { appDraw = drawMenu
+          , appChooseCursor = showFirstCursor
           , appHandleEvent = handleEvent
           , appStartEvent = return
-          , appAttrMap = const boardAttrMap
+          , appAttrMap = const menuAttrMap
           }
 
-boardAttrMap :: AttrMap
-boardAttrMap = attrMap V.defAttr 
+---------- Brick Attribute Map ----------
+
+menuAttrMap :: AttrMap
+menuAttrMap = attrMap V.defAttr
   [
     (focusedFormInputAttr, black `on` yellow)
-  , (invalidFormInputAttr, white `on` red)  
+  , (invalidFormInputAttr, white `on` red)
   ]
 
+---------- Handle Brick Events ------------
+
 handleEvent :: MenuState -> BrickEvent MenuResourceName MenuEvent -> EventM MenuResourceName (Next MenuState)
-handleEvent g _ = continue g
+
+handleEvent s (AppEvent (ConnectionSuccess c)) = error "Not implemented"
+handleEvent s (AppEvent (ConnectionFailed e)) = continue (s & connectionState .~ Error e)
+
+handleEvent s (VtyEvent (V.EvKey V.KEnter [])) = connectToTable s
+handleEvent s (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt s
+handleEvent s ev = do
+  newForm <- handleFormEvent ev (s ^. connectionInfoForm)
+  continue (s & connectionInfoForm .~ newForm)
+
+
+connectToTable :: MenuState -> EventM MenuResourceName (Next MenuState)
+connectToTable s = liftIO (requestConnectToTable (s ^. connectionInfo)) >> continue (s & connectionState .~ Connecting)
+
+
+
+requestConnectToTable :: connectionInfo -> IO ()
+-- TODO
+requestConnectToTable ci = return ()
+
+---------- Brick Drawing --------------
 
 drawMenu :: MenuState -> [Widget MenuResourceName]
-drawMenu = error "Not implemented"
+drawMenu s = [vBox
+              [
+                C.hCenter $ str "Connect to Table"
+              , vLimit 1 $ drawStatus s
+              , padTop (Pad 2) $ C.center $ renderForm $ s ^. connectionInfoForm
+              , str "Press Tab to switch fields"
+              , str "Press enter to confirm"
+              , str "Press q to exit"
+              ]
+            ]
 
-renderForm (Form s e n)
+drawStatus :: MenuState -> Widget n
+drawStatus s = case s ^. connectionState of
+  Idle -> str ""
+  Connecting -> str "Connecting.... "
+  Error s -> str ("Error: " ++ s)
+  Success -> str "Success fully connected to table"
 
--- https://github.com/jtdaugherty/brick/blob/master/docs/guide.rst#input-forms
-
+------- Brick Input form creation -----------
 makeConnectionForm :: ConnectionInfo -> Form ConnectionInfo MenuEvent MenuResourceName
-makeConnectionForm = 
+makeConnectionForm =
     newForm [
-          editTextField serverIP MenuServerIPField (Just 0)
-        , editTextField tableID MenuTableIDField (Just 1)
+          (str "Server IP: " <+>) @@= editTextField serverIP MenuServerIPField (Just 1)
+        , (str "Table ID: " <+>) @@= editTextField tableID MenuTableIDField (Just 1)
     ]
